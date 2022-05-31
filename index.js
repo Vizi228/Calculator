e036e42ea1bb9455eb7b3973b1fde3c2be8c542a
Oleh Yelechko <lalka210767@gmail.com>
2022-06-01 00:56:22 +0300

    add C btn and replace result block

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, '.', 0, '='];
-const actions = ['*', '/', '+', '-']
+const actions = ['*', '/', '+', '-', 'C']
 const numbersContainer = document.querySelector('.numbers');
 const actionsContainer = document.querySelector('.add-buttons')
 function onLoad() {
@@ -21,7 +21,6 @@ onLoad()
 const buttons = document.querySelectorAll('.calc-btn');
 const addButtons = document.querySelectorAll('.add-buttons span');
 const result = document.querySelector('.result');
-const lastResult = document.querySelector('.last-result')
 buttons.forEach(item => {
     item.addEventListener('click', onClickCalcBtn)
 })
@@ -32,11 +31,11 @@ let prevValue = null;
 let currentAction = null;
 function onClickCalcBtn(e) {
     if (e.target.innerHTML === '=') {
-        if (currentAction === '+') lastResult.innerHTML = +prevValue + +result.innerHTML;
-        if (currentAction === '-') lastResult.innerHTML = +prevValue - +result.innerHTML;
-        if (currentAction === '/') lastResult.innerHTML = +prevValue / +result.innerHTML;
-        if (currentAction === '*') lastResult.innerHTML = +prevValue * +result.innerHTML;
-        result.innerHTML = '';
+        if (!currentAction) return
+        if (currentAction === '+') result.innerHTML = +prevValue + +result.innerHTML;
+        if (currentAction === '-') result.innerHTML = +prevValue - +result.innerHTML;
+        if (currentAction === '/') result.innerHTML = +prevValue / +result.innerHTML;
+        if (currentAction === '*') result.innerHTML = +prevValue * +result.innerHTML;
         prevValue = null;
         currentAction = null;
         return
@@ -47,7 +46,13 @@ function onClickCalcBtn(e) {
     result.innerHTML += e.target.innerHTML;
 }
 function onClickAddBtn(e) {
-    if (prevValue) return
+    if (e.target.innerHTML === 'C') {
+        result.innerHTML = '';
+        currentAction = null;
+        prevValue = null;
+        return
+    }
+    if (prevValue) return;
     prevValue = result.innerHTML;
     result.innerHTML = e.target.innerHTML;
     currentAction = e.target.innerHTML;
